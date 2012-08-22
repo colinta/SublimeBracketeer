84f2ad3efeb3e1e5e5cce8076febc13d1a525b71
Colin Thomas-Arnold <colinta@gmail.com>
2012-08-22 14:01:59 -0600

    more fixes

--- bracketeer.py
+++ bracketeer.py
@@ -212,19 +212,19 @@ class BracketeerIndentCommand(sublime_plugin.TextCommand):
 
 
 class BracketeerBracketMatcher(sublime_plugin.TextCommand):
-    def find_brackets(self, region, search_brackets=None):
+    def find_brackets(self, region, closing_search_brackets=None):
         match_map = {
             '}': '{',
             ']': '[',
             ')': '(',
             }
-        # find next brace in search_brackets
-        if not search_brackets:
-            search_brackets = ['}', ']', ')']
-        elif isinstance(search_brackets, basestring):
-            search_brackets = [search_brackets]
+        # find next brace in closing_search_brackets
+        if not closing_search_brackets:
+            closing_search_brackets = ['}', ']', ')']
+        elif isinstance(closing_search_brackets, basestring):
+            closing_search_brackets = [closing_search_brackets]
 
-        closing_search_brackets = [match_map[bracket] for bracket in search_brackets]
+        opening_search_brackets = [match_map[bracket] for bracket in closing_search_brackets]
         begin_point = region.begin() - 1
         end_point = region.end()
 
@@ -235,7 +235,7 @@ class BracketeerBracketMatcher(sublime_plugin.TextCommand):
             # if the current character is a bracket, and the character to the left is the
             # *matching* bracket, don't match the empty contents
             c = self.view.substr(end_point)
-            if c in search_brackets and self.view.substr(end_point - 1) == match_map[c]:
+            if c in closing_search_brackets and self.view.substr(end_point - 1) == match_map[c]:
                 # cursor is between two brackets - select them and return
                 return Region(begin_point, end_point + 1)
 
@@ -244,17 +244,18 @@ class BracketeerBracketMatcher(sublime_plugin.TextCommand):
             c1 = self.view.substr(begin_point)
             c2 = self.view.substr(end_point)
 
-            if c2 in search_brackets and c1 == match_map[c2]:
+            if c2 in closing_search_brackets and c1 == match_map[c2]:
                 return Region(begin_point, end_point + 1)
 
         # scan forward searching for a closing bracket.
+        started_in_string = bool(self.view.score_selector(end_point, 'string') or self.view.score_selector(begin_point, 'string'))
         bracket_count = 0
         while True:
             c = self.view.substr(end_point)
-            if not self.view.score_selector(end_point, 'string'):
-                if bracket_count <= 0 and c in search_brackets:
+            if started_in_string or not self.view.score_selector(end_point, 'string'):
+                if bracket_count <= 0 and c in closing_search_brackets:
                     break
-                elif c in search_brackets and c in OPENING_BRACKETS:
+                elif c in opening_search_brackets and c in OPENING_BRACKETS:
                     bracket_count += 1
                 elif c in closing_search_brackets and c in CLOSING_BRACKETS:
                     bracket_count -= 1
@@ -270,12 +271,12 @@ class BracketeerBracketMatcher(sublime_plugin.TextCommand):
         look_for = match_map[c]
         while True:
             c = self.view.substr(begin_point)
-            if self.view.score_selector(begin_point, 'string') == 0:
+            if started_in_string or not self.view.score_selector(begin_point, 'string'):
                 if bracket_count == 0 and c == look_for:
                     break
-                elif c in OPENING_BRACKETS:
+                elif c in opening_search_brackets and c in OPENING_BRACKETS:
                     bracket_count += 1
-                elif c in CLOSING_BRACKETS:
+                elif c in closing_search_brackets and c in CLOSING_BRACKETS:
                     bracket_count -= 1
             begin_point -= 1
             if begin_point < 0:
